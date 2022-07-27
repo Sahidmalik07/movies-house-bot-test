b2ddde09c5892b5c130bd18cdd888a0bda2a3d2d
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-07-27 17:30:26 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -859,7 +859,7 @@ async def auto_filter(client, msg, spoll=False):
         )
     btn.insert(0, [
         InlineKeyboardButton('♻️ Join Our Group ♻️', url='https://youtube.com/channel/UCPaHDqWf3D3w2nxb8p3sr4A'),
-        InlineKeyboardButton("MALIK ⏩", callback_data=f"malik_{offset}")
+        InlineKeyboardButton("MALIK ⏩", callback_data=f"malik_{ooffset}")
     ])
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     TEMPLATE = settings['template']
