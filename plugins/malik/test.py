ab78a9dcbd0f14c4bfaf78cba4022d8fe54e934a
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-07-27 16:40:30 +0530

    Delete test.py

--- plugins/malik/test.py
+++
@@ -1,9 +0,0 @@
-#sahid malik
-import offset
-from pyrogram import Client, filters
-
-@Client.on_callback_query(filters.regex(r"^malik"))
-async def next_page(bot, query):
-    ident, req, key, b_offset = query.data.split("_")
-    if int(req) not in [query.from_user.id, 0]:
-        return await query.answer(f"⚠️ Hey, {query.from_user.first_name}.. \n\nSearch Your Own File, \n\n⚠️ Don't Click Others Results 😬", show_alert=True)
