1d472b775f2c9b05aef0b16aaa4e5d5bc682840b
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-07-27 17:18:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1045,6 +1045,6 @@ async def manual_filters(client, message, text=False):
 @Client.on_callback_query(filters.regex(r"^malik"))
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
-    if int(req) not in [query.from_user.id, 0]:
+    if int(req) not in [query.from_ADMIN.id, 0]:
         return await query.answer(f"⚠️ Hey, {query.from_user.first_name}.. \n\nSearch Your Own File, \n\n⚠️ Don't Click Others Results 😬", show_alert=True)
 
