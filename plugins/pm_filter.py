4d0aefdc77223901bbf433ad174465a1e760edd6
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-07-27 16:18:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -3,6 +3,7 @@ import asyncio
 import re
 import ast
 
+from plugins.malik.test import malik
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script, ALURT_FND, M_NT_FND, M_NNT_FND, M_NNT_FNDD
 import pyrogram
