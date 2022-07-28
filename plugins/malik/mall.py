d5b90c93440334d0ff3909a6aed74de79d95f5c0
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-07-28 14:29:57 +0530

    Create mall.py

---
+++ plugins/malik/mall.py
@@ -0,0 +1,3 @@
+elif query.data == "Hello":
+        await query.answer("GROUP RULES \n☀️ Search With Correct Spelling.. \n☀️ Try to Search movie With  : Gotham S03E01 And S03E10..\n\n☀️Don't Do Any Self Promotion.\n\n☀️ Don't Send Any K..", show_alert=True)
+        return
