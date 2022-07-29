d94f5571c2d70623176cd114fc1d7cacfe777fff
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-07-29 21:54:42 +0530

    Update extra.py

--- plugins/malik/extra.py
+++ plugins/malik/extra.py
@@ -66,7 +66,7 @@ async def star(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_text(
-            text=(GHHMT),
+            text=(GHHMT.format(total_users)),
             reply_markup=reply_markup,
             parse_mode='html'
         )
@@ -85,6 +85,21 @@ async def get_ststs(bot, message):
                ),
                parse_mode='html'
 )
+@Client.on_message(filters.command('botinfo') & filters.incoming)
+async def get_ststs(bot, message):
+    malik = await message.reply('Wait..')
+    total_users = await db.total_users_count()
+    await malik.edit(
+               text=(GHHMT.format(total_users)),
+               reply_markup=InlineKeyboardMarkup(
+                                      [[
+                                        InlineKeyboardButton('🌐 Add Me To Your Groups 🌐', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                                      ]]
+               ),
+               parse_mode='html'
+)
+
+
 # Ban py
 
 @Client.on_message(filters.command("ban"))
