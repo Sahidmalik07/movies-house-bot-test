c70a48a0a5b084db15c3f36cdf6c776582fe217a
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-07-29 22:39:25 +0530

    Update extra.py

--- plugins/malik/extra.py
+++ plugins/malik/extra.py
@@ -67,7 +67,7 @@ async def star(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=(GHHMN),
-            caption=(GHHMM.format(query.from_user.first_name)),
+            caption=(GHHMM.format(message.from_user.mention)),
             reply_markup=reply_markup,
             parse_mode='html'
         )
